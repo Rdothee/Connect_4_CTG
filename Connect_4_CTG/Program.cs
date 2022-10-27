a7c945b177ce0ed9681634d850a3bb582d398cd3
Rdothee <robbe.dothee@hotmail.com>
2022-10-27 14:37:50 +0200

    added Connect_4 class

--- Connect_4_CTG/Program.cs
+++ Connect_4_CTG/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+using Connect_4_CTG;
+
+internal class Program
+{
+    static void Main(string[] args)
+
+    {
+        Connect_4.Start();
+    }
+}
